242f38cc30880efb3c9d762d8055053325bc1053
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2023-11-28 23:19:30 +0900

    Title: 만나는 그 순간, Time: 9ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 231128/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void route(vector<int>& arr, int n)
+{
+    int current = 0;
+    int second = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        char dir; int dist;
+        cin >> dir >> dist;
+
+        int sign = dir == 'L' ? -1 : 1;
+        for (int j = 0; j < dist; j++)
+        {
+            current += sign;
+            arr[second++] = current;
+        }
+    }
+
+}
+
+int main() {
+    const int MAX = 1000 * 1000 + 1;
+
+    int n, m;
+    cin >> n >> m;
+
+    int currentA = 0;
+    int second = 0;
+    vector<int> routeA(MAX, 0);
+    route(routeA, n);
+    
+    vector<int> routeB(MAX, 0);
+    route(routeB, m);
+
+    for (int i = 0; i < MAX; i++)
+    {
+        if (routeA[i] == routeB[i])
+        {
+            cout << i + 1;
+            break;
+        }
+    }
+
+    return 0;
+}
