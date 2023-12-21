7fbfb4f22d4340852315e7ee6005416ed1152d8e
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2023-12-22 00:04:34 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231222/계단 오르기/climbing-stairs.cpp
+++ 231222/계단 오르기/climbing-stairs.cpp
@@ -7,8 +7,8 @@ int main() {
 
     const int MAX = 1000;
     int dp[MAX + 1] = {0};
-    dp[0] = dp[2] = 1;
-    for (int i = 3; i <= n; i++)
+    dp[2] = dp[3] = 1;
+    for (int i = 4; i <= n; i++)
         dp[i] = dp[i - 3] + dp[i - 2];
 
     cout << dp[n] % 10007;
