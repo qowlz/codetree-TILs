95308b45017511b55e146193009893626630d710
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2023-12-22 00:00:24 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231222/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    const int MAX = 1000;
+    int dp[MAX + 1] = {0};
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++)
+        dp[i] = dp[i - 3] + dp[i - 2];
+
+    cout << dp[n] % 10007;
+
+    return 0;
+}
