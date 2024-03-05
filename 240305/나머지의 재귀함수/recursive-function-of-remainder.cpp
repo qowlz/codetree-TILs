3b03ea74d3b85231539ce522d37007c468929e75
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-03-05 21:47:35 +0900

    Title: 나머지의 재귀함수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/나머지의 재귀함수/recursive-function-of-remainder.cpp
+++ 240305/나머지의 재귀함수/recursive-function-of-remainder.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 vector<int> cached;
 
-long long solve(int n)
+int solve(int n)
 {
     if (n <= 0) return 0;
     if (cached[n]) return cached[n];
@@ -17,7 +16,7 @@ int main() {
     int n;
     cin >> n;
 
-    cached.resize(max(n, 3));
+    cached.resize(n + 2);
     cached[1] = 2; cached[2] = 4;
     cout << solve(n);
 
