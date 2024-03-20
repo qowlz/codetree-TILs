cca86860c718a30e3a13ed43964d2ecd54eb6f6c
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-03-20 13:59:08 +0900

    Title: 알파벳과 사칙연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240320/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 240320/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -19,16 +19,20 @@ int calc(int a, int b, char op)
     return a + b;
 }
 
+int getIdeal(int acc, char op)
+{
+    if (op == '+' || (acc >= 0 && op == '*')) return 4;
+
+    return 1;
+}
+
 int solve(int idx, int acc)
 {
     if (idx + 2 >= ex.length()) return acc;
 
-    int ret = -2147483647 - 1;
-    for (int i = 1; i <= 4; i++)
-    {
-        char op = inRange(idx + 1) ? ex[idx + 1] : '+';
-        ret = max(ret, solve(idx + 2, calc(acc, i, op)));
-    }
+    char op = inRange(idx + 1) ? ex[idx + 1] : '+';
+    int b = getIdeal(acc, op);
+    int ret = max(ret, solve(idx + 2, calc(acc, b, op)));
 
     return ret;
 }
