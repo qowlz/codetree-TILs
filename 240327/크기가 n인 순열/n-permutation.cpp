c6242c4782666ad4e5bd3121b58c4bcb9d1e620f
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-03-27 12:40:39 +0900

    Title: 크기가 n인 순열, Time: 43ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240327/크기가 n인 순열/n-permutation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+vector<int> pick;
+vector<bool> visit;
+
+void solve()
+{
+    if (pick.size() >= N)
+    {
+        for (int i = 0; i < pick.size(); i++)
+            cout << pick[i] << ' ';
+        cout << '\n';
+
+        return;
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (visit[i]) continue;
+
+        pick.push_back(i);
+        visit[i] = true;
+        solve();
+        visit[i] = false;
+        pick.pop_back();
+    }
+
+}
+
+int main() {
+    cin >> N;
+
+    visit.assign(N + 1, false);
+    solve();
+
+    return 0;
+}
