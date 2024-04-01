bdb08c5b9775d65d6556c0fc3235c14a05f899dc
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-04-01 11:41:33 +0900

    Title: 외판원 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240401/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<vector<int>> cost;
+vector<int> pick;
+vector<bool> visit;
+
+int solve(int acc)
+{
+    if (pick.size() >= N - 1) return acc + cost[pick.back()][0];
+
+    int current = pick.size() > 0 ? pick.back() : 0;
+    int ret = INT_MAX;
+    for (int i = 1; i < N; i++)
+    {
+        if (visit[i]) continue;
+
+        pick.push_back(i); 
+        visit[i] = true;
+        ret = min(ret, solve(acc + cost[current][i]));
+        pick.pop_back();
+        visit[i] = false;
+    }
+
+    return ret;
+}
+
+int main() {
+    cin >> N;
+    
+    cost.assign(N, vector<int>(N, 0));
+    visit.assign(N, false);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> cost[i][j];
+
+    cout << solve(0);
+
+    return 0;
+}
