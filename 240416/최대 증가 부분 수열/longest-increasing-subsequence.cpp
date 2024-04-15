c9856784630d8ce00ced343cf6e582ce5102d686
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-04-16 01:47:02 +0900

    Title: 최대 증가 부분 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240416/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    vector<int> dp(n, 0);
+    dp[n - 1] = 1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] <= arr[i]) continue;
+
+            dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+
+    cout << dp[0];
+
+    return 0;
+}
