4650123b6b74884b7924f8493f6b72392c98fda6
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-04-16 15:17:57 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240416/사각형 채우기/rectangle-fill.cpp
+++ 240416/사각형 채우기/rectangle-fill.cpp
@@ -9,7 +9,7 @@ int main() {
     vector<int> dp(n + 1, 0);
     dp[1] = 1; dp[2] = 2;
     for (int i = 3; i <= n; i++)
-        dp[i] = dp[i -1] + dp[i - 2];
+        dp[i] = dp[i -1] % 10007 + dp[i - 2] % 10007;
 
     cout << dp[n] % 10007;
 
