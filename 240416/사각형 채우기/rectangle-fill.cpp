56bfae6c16991f6eef3c7d94ab9d757878145846
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-04-16 15:16:22 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240416/사각형 채우기/rectangle-fill.cpp
+++ 240416/사각형 채우기/rectangle-fill.cpp
@@ -9,21 +9,9 @@ int main() {
     vector<int> dp(n + 1, 0);
     dp[1] = 1; dp[2] = 2;
     for (int i = 3; i <= n; i++)
-        dp[i] = dp[i -1] % 10007 + dp[i - 2] % 10007;
+        dp[i] = dp[i -1] + dp[i - 2];
 
-    cout << dp[n];
-
-    // dp[i] = 2xi의 경우를 채울 수 있는 방법의 수
-    // dp[i] = dp[i - 1] + dp[i - 2];
-
-    // 2 x 1인 경우
-        // 2 x 1로 밖에 못채운다
-        // 총 1가지
-    // 2 x 2인 경우
-        // 2 x 1 2개
-        // 1 x 2 2개
-        // 총 2가지
-    // 2 x 3인 경우
+    cout << dp[n] % 10007;
 
 
     return 0;
