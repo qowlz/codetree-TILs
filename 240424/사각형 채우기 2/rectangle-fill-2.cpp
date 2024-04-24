6b87216437425d717dbf383f358394e1c370b97c
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-04-24 16:36:59 +0900

    Title: 사각형 채우기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240424/사각형 채우기 2/rectangle-fill-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1; dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = 2 * dp[i - 2] + dp[i - 1];
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
