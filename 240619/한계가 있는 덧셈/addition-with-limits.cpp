a87763ccb8d4a555c30cb34b4e9b6b94c44f21e8
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-06-19 00:31:54 +0900

    Title: 한계가 있는 덧셈, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240619/한계가 있는 덧셈/addition-with-limits.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, K;
+vector<int> arr;
+
+// 4 8
+// 1 5 3 2
+// 1 - 5 - 3 - 2
+// 5 - 3 - 2
+int solve(int idx, int acc, int score)
+{
+    if (idx >= N) return score;
+
+    if (idx >= 0) acc += arr[idx];
+
+    int get = 0;
+    if (acc >= K)
+    {
+        get = acc - K;
+        score += get;
+        acc = 0;
+    }
+
+    if (get > 0) 
+        return max(solve(idx + 1, acc, score), solve(idx + 2, acc, score));
+    else 
+        return solve(idx + 1, acc, score)
+}
+
+
+int main() {
+    cin >> N >> K;
+
+    arr.assign(N, 0);
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    
+    cout << solve(-1, 0, 0);
+
+    return 0;
+}
