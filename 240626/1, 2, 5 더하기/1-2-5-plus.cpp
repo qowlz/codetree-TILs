63956cf3f09df7f112cbeae050032e17ef687a79
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-06-26 14:20:41 +0900

    Title: 1, 2, 5 더하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240626/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MOD 10007
+
+int N;
+
+int main() {
+    cin >> N;
+
+    int nums[] = {1, 2, 5};
+    vector<int> dp(N + 1, 0);
+    dp[0] = 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (i - nums[j] >= 0)
+                dp[i] += dp[i - nums[j]] % MOD;
+        }
+    }
+
+    cout << dp[N] << endl;
+
+
+    // dp[1] = 1 + 0
+    // dp[2] = 1 + 1, 2 + 0
+    // dp[3] = 1 + 1 + 1, 1 + 2, 2 + 1
+    // dp[4] = 1 + 1 + 1 + 1, 1 + 1 + 2, 1 + 2 + 1, 
+
+    // (dp[2] - (dp[1] * dp[2 - 1])) * dp[n - 2]
+    // 1
+    // (dp[5])
+
+    // dp[1] * dp[n - 1], dp[n - 2], dp[n - 5]
+    // 5 + 3 + 1
+    
+    
+
+    return 0;
+}
