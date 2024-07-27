ed2b759b1f34325f59090be2541d4f19d7c0f580
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-07-27 20:06:30 +0900

    Title: 두 수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240727/두 수의 합/sum-of-two-num.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    unordered_map<long long, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        m[num] += 1;
+    }
+
+    int answer = 0;
+    for (const auto& item : m)
+    {
+        long long sub = k - item.first;   
+        if (m[sub]) answer += m[sub];
+    }
+
+    cout << answer / 2;
+
+    return 0;
+}
