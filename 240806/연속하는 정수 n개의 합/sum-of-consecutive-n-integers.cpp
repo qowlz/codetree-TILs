272549b0c73a61a78f5a324823326948630c72ea
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-08-06 18:39:09 +0900

    Title: 연속하는 정수 n개의 합, Time: 36ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/연속하는 정수 n개의 합/sum-of-consecutive-n-integers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    int sum = 0;
+    int j = 0;
+    int answer = 0;
+    for (int i = 0; i < n; i++)
+    {
+        while (j < n)
+        {
+            if (sum >= m) break;
+            else sum += arr[j++];
+        }
+
+        if (sum == m) answer++;
+
+        sum -= arr[i];
+    }
+
+    cout << answer;
+
+    return 0;
+}
