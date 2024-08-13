113d7822fdff5ab68c48db39997fbfa28e005d9e
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-08-13 15:24:48 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240813/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.cpp
+++ 240813/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.cpp
@@ -6,6 +6,29 @@ using namespace std;
 
 int N, D;
 
+int getDiffY(int minY, int maxY)
+{
+    if (minY == INT_MAX || maxY == INT_MIN) return 0;
+
+    return abs(maxY - minY);
+}
+
+void updateMinY(int test, int& minY, int& prev)
+{
+    if (minY <= test) return;
+
+    prev = minY;
+    minY = test;
+}
+
+void updateMaxY(int test, int& maxY, int& prev)
+{
+    if (maxY >= test) return;
+
+    prev = maxY;
+    maxY = test;
+}
+
 int main() {
     cin >> N >> D;
 
@@ -14,21 +37,32 @@ int main() {
     {
         int x, y;
         cin >> x >> y;
-        arr.push_back({y, x});
+        arr.push_back({x, y});
     }
 
     sort(arr.begin(), arr.end());
 
-    int l = 0;
-    int r = N - 1;
+    int j = -1;
+    int minY = INT_MAX;
+    int prevMin = INT_MAX;
+    int maxY = INT_MIN;
+    int prevMax = INT_MIN;
     int answer = INT_MAX;
-    while (l < r)
+    for (int i = 0; i < N; i++)
     {
-        int diff = abs(arr[r].first - arr[l].first);
-        if (diff < D) break;
+        while (j + 1 < N && getDiffY(minY, maxY) < D)
+        {
+            j += 1;
+            updateMinY(arr[j].second, minY, prevMin);
+            updateMaxY(arr[j].second, maxY, prevMax);
+        }
 
-        answer = min(answer, abs(arr[r].second - arr[l].second));
-        r--;
+        if (getDiffY(minY, maxY) >= D)
+            answer = min(answer, arr[j].first - arr[i].first);
+
+
+        minY = prevMin;
+        maxY = prevMax;
     }
 
     if (answer == INT_MAX)
