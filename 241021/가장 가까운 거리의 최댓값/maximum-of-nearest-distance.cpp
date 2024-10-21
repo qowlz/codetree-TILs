3b3072adb5f3a14bf61490aca702387a490d46c4
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-10-21 21:57:17 +0900

    Title: 가장 가까운 거리의 최댓값, Time: 1097ms, Memory: 5MB, Status: Time Limit Exceed - CodeTree

---
+++ 241021/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+#define MAX_N 100000
+#define INF 1e9
+
+int n, m;
+vector<pair<int, int>> g[MAX_N + 1];
+
+vector<int> dijkstra(int s)
+{
+    vector<int> dist(n + 1, INF);
+    dist[s] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    for (int i = 1; i <= n; i++)
+        pq.push(make_pair(dist[i], i));
+        
+
+    while(!pq.empty())
+    {
+        int d, u;
+        tie(d, u) = pq.top();
+        pq.pop();
+
+        if (d != dist[u]) continue;
+
+        for (int i = 0; i < g[u].size(); i++)
+        {
+            int v, w;
+            tie(v, w) = g[u][i];
+            if (d + w >= dist[v]) continue;
+
+            dist[v] = d + w;
+            pq.push(make_pair(dist[v], v));
+        }
+    }
+
+    return dist;
+}
+
+int main() {
+    cin >> n >> m;
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    // 그래프 구성
+    while (m--)
+    {
+        int s, e, w;
+        cin >> s >> e >> w;
+        g[s].push_back(make_pair(e, w));
+        g[e].push_back(make_pair(s, w));
+    }
+
+    int ans = 0;    
+    for (int s = 1; s <= n; s++)
+    {
+        auto dist = dijkstra(s);
+        int closest = min(dist[a], dist[b]);
+        closest = min(closest, dist[c]);
+        ans = max(ans, closest);
+    }
+
+    cout << ans;
+
+    return 0;
+}
