ec90740ca7d693f5ced3eacf01ad9470739572f7
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-10-23 05:01:33 +0900

    Title: 선분 위의 점, Time: 365ms, Memory: 1MB, Status: Passed - CodeTree

--- 241023/선분 위의 점/point-on-the-line-segment.cpp
+++ 241023/선분 위의 점/point-on-the-line-segment.cpp
@@ -6,44 +6,6 @@ using namespace std;
 int n, m;
 vector<int> points;
 
-int lowerBound(int x)
-{
-    int left = 0, right = n - 1;
-    int minIdx = n;
-    while (left <= right)
-    {
-        int mid = (left + right) / 2;
-        if (points[mid] >= x)
-        {
-            right = mid - 1;
-            minIdx = min(minIdx, mid);
-        }
-        else
-            left = mid + 1;
-    }
-
-    return minIdx;
-}
-
-int upperBound(int x)
-{
-    int left = 0, right = n - 1;
-    int minIdx = n;
-    while (left <= right)
-    {
-        int mid = (left + right) / 2;
-        if (points[mid] > x)
-        {
-            right = mid - 1;
-            minIdx = min(minIdx, mid);
-        }
-        else
-            left = mid + 1;
-    }
-
-    return minIdx;
-}
-
 int main()
 {
     cin >> n >> m;
@@ -58,7 +20,7 @@ int main()
     {
         int x1, x2;
         cin >> x1 >> x2;
-        cout << upperBound(x2) - lowerBound(x1) << '\n';
+        cout << upper_bound(points.begin(), points.end(), x2) - lower_bound(points.begin(), points.end(), x1) << '\n';
     }
 
     return 0;
