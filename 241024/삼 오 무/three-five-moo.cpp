ea7463054f0eb79b4c77b587db9081274979071d
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-10-24 02:19:50 +0900

    Title: 삼 오 무, Time: 75ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241024/삼 오 무/three-five-moo.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int getThree(long long num)
+{
+    int ret = 0;
+    int i = 1;
+    while (3 * i <= num)
+    {
+        ret++;
+        i++;
+    }
+
+    return ret;
+}
+
+int getFive(long long num)
+{
+    int ret = 0;
+    int i = 1;
+    while (5 * i <= num)
+    {
+        ret++;
+        i++;
+    }
+
+    return ret;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    long long left = 1, right = 200000000;
+    long long ans = 0;
+    while (left <= right)
+    {
+        long long mid = (left + right) / 2;
+        
+        long long order = mid - getThree(mid) - getFive(mid);
+        if (order >= n)
+        {
+            right = mid - 1;
+            ans = mid;
+        }
+        else
+            left = mid + 1;
+    }
+
+    cout << ans;
+
+    return 0;
+}
