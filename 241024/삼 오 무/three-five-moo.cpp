40f5afaf767e3696f216cba510404ab384a4c8c0
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-10-24 02:42:28 +0900

    Title: 삼 오 무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241024/삼 오 무/three-five-moo.cpp
+++ 241024/삼 오 무/three-five-moo.cpp
@@ -1,43 +1,22 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
-int getThree(long long num)
+int getMoo(int num)
 {
-    int ret = 0;
-    int i = 1;
-    while (3 * i <= num)
-    {
-        ret++;
-        i++;
-    }
-
-    return ret;
-}
-
-int getFive(long long num)
-{
-    int ret = 0;
-    int i = 1;
-    while (5 * i <= num)
-    {
-        ret++;
-        i++;
-    }
-
-    return ret;
+    return num / 3 + num / 5 - num / 15;
 }
 
 int main() {
     int n;
     cin >> n;
     
-    long long left = 1, right = 200000000;
+    int left = 1, right = INT_MAX;
     long long ans = 0;
     while (left <= right)
     {
         long long mid = (left + right) / 2;
-        
-        long long order = mid - getThree(mid) - getFive(mid);
+        long long order = mid - getMoo(mid);
         if (order == n)
         {
             ans = mid;
