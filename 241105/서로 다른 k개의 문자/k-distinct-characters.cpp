3b3c4c51a9295ca13cbd4d275e9645970c728e85
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-11-05 04:26:56 +0900

    Title: 서로 다른 k개의 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241105/서로 다른 k개의 문자/k-distinct-characters.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+string str;
+int k;
+
+int main() {
+    cin >> str >> k;
+
+    int ans = 0;
+    int j = 0;
+    unordered_set<char> s;
+    for (int i = 0; i < str.length(); i++)
+    {
+        while (j < str.length() && s.size() <= k)
+        {
+            s.insert(str[j]);
+            j++;
+        }
+
+        ans = max(ans, j - i);
+
+        s.erase(str[i]);
+    }
+
+    cout << ans;
+
+    return 0;
+}
