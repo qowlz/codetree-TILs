e591b44677cbfcc88fa7d7918efe54544a0878ba
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-11-04 19:07:14 +0900

    Title: 트리의 지름, Time: 155ms, Memory: 6MB, Status: Passed - CodeTree

--- 241104/트리의 지름/diameter-of-tree.cpp
+++ 241104/트리의 지름/diameter-of-tree.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 using namespace std;
 
 #define MAX_N 100000
 
 int n;
 vector<pair<int, int>> arr[MAX_N + 1];
-int far_v = 0;
+vector<bool> visited;
+int far_v = 1;
 int far_d = 0;
 
 void traversal(int x, int d)
 {
+    visited[x] = true;
     if (d > far_d)
     {
         far_v = x;
@@ -19,7 +22,10 @@ void traversal(int x, int d)
 
     for (int i = 0; i < arr[x].size(); i++)
     {
-        traversal(arr[x][i].first, d + arr[x][i].second);
+        int to, w;
+        tie(to, w) = arr[x][i];
+
+        if (!visited[to]) traversal(to, d + w);
     }
 }
 
@@ -31,11 +37,14 @@ int main() {
         int s, e, w;
         cin >> s >> e >> w;
         arr[s].push_back({e, w});
+        arr[e].push_back({s, w});
     }
 
+    visited.assign(n + 1, false);
     traversal(1, 0);
 
     far_d = 0;
+    visited.assign(n + 1, false);
     traversal(far_v, 0);
 
     cout << far_d;
