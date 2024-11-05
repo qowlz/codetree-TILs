85c64f9060f30ff2b065525796e7131e2b58096f
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-11-06 03:54:59 +0900

    Title: 특정 조건으로 묶여있는 숫자 그룹, Time: 1096ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241106/특정 조건으로 묶여있는 숫자 그룹/a-group-of-numbers-tied-to-specific-conditions.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+vector<int> arr;
+
+int main() {
+    cin >> n >> k;
+
+    arr.assign(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int j = -1;
+    vector<pair<int, int>> groups;
+    for (int i = 0; i < n; i++)
+    {
+        while(j + 1 < n && arr[j + 1] - arr[i] <= k) j++;
+
+        groups.push_back({i, j});
+    }
+
+    int ans = 0;
+    for (int i = 0; i < groups.size(); i++)
+    {
+        for (int j = i + 1; j < groups.size(); j++)
+        {
+            if (groups[j].first > groups[i].second || groups[j].second < groups[i].first)
+            {
+                int a = groups[i].second - groups[i].first + 1;
+                int b = groups[j].second - groups[j].first + 1;
+                ans = max(ans, a + b);
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
