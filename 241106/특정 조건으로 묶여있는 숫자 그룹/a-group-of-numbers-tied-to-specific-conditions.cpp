8d5fb9c384ced040e8be7ec6b4318f8d17c3b0ce
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-11-06 06:19:26 +0900

    Title: 특정 조건으로 묶여있는 숫자 그룹, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241106/특정 조건으로 묶여있는 숫자 그룹/a-group-of-numbers-tied-to-specific-conditions.cpp
+++ 241106/특정 조건으로 묶여있는 숫자 그룹/a-group-of-numbers-tied-to-specific-conditions.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdio>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -8,43 +7,39 @@ int n, k;
 vector<int> arr;
 
 int main() {
-    // cin >> n >> k;
-    scanf("%d %d", &n, &k);
+    cin >> n >> k;
 
     arr.assign(n, 0);
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &arr[i]);
-        // cin >> arr[i];
+        cin >> arr[i];
     }
 
     sort(arr.begin(), arr.end());
 
     int j = -1;
     vector<pair<int, int>> groups;
+    vector<int> l(n, 1);
     for (int i = 0; i < n; i++)
     {
         while(j + 1 < n && arr[j + 1] - arr[i] <= k) j++;
+
+        l[j] = max(l[j], j - i + 1);
 
         groups.push_back({i, j});
     }
 
     int ans = 0;
-    for (int i = 0; i < groups.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < groups.size(); j++)
-        {
-            if (groups[j].first > groups[i].second || groups[j].second < groups[i].first)
-            {
-                int a = groups[i].second - groups[i].first + 1;
-                int b = groups[j].second - groups[j].first + 1;
-                ans = max(ans, a + b);
-            }
-        }
+        int a = groups[i].second - groups[i].first + 1;
+        if (groups[i].first > 0)
+            ans = max(ans, a + l[groups[i].first - 1]);
+        else
+            ans = max(ans, a);
     }
 
-    // cout << ans;
-    printf("%d", ans);
+    cout << ans;
 
     return 0;
 }
