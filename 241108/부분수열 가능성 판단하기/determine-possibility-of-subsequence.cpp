9b20c327b89bed19d5b2c3f7a81d72217b90ac28
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-11-08 08:50:30 +0900

    Title: 부분수열 가능성 판단하기, Time: 57ms, Memory: 0MB, Status: Passed - CodeTree

--- 241108/부분수열 가능성 판단하기/determine-possibility-of-subsequence.cpp
+++ 241108/부분수열 가능성 판단하기/determine-possibility-of-subsequence.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
 
 int n, m;
@@ -9,28 +10,27 @@ vector<int> b;
 int main() {
     cin >> n >> m;
 
-    a.assign(n, 0);
-    for (int i = 0; i < n; i++)
+    a.assign(n + 1, 0);
+    for (int i = 1; i <= n; i++)
         cin >> a[i];
     
-    b.assign(m, 0);
-    for (int i = 0; i < m; i++)
+    b.assign(m + 1, 0);
+    for (int i = 1; i <= m; i++)
         cin >> b[i];
-    
 
-    vector<int> L(m, 0);
-    int j = 0;
-    for (int i = 0; i < m; i++)
+    vector<int> L(m + 2, -1);
+    int j = 1;
+    for (int i = 1; i <= m; i++)
     {
-        while(j < n && a[j] != b[i])
+        while(j <= n && a[j] != b[i])
             j++;
 
         L[i] = j++;
     }
 
-    vector<int> R(m, 0);
-    j = n - 1;
-    for (int i = m - 1; i >= 0; i--)
+    vector<int> R(m + 2, n + 1);
+    j = n;
+    for (int i = m; i >= 1; i--)
     {
         while(j >= 0 && a[j] != b[i])
             j--;
@@ -38,25 +38,20 @@ int main() {
         R[i] = j--;
     }
 
-    // for (int i = 0; i < m; i++)
+    // for (int i = 1; i <= m; i++)
     // {
     //     cout << "L[" << i << "]: " << L[i] << endl;
     // }
 
-    // for (int i = 0; i < m; i++)
+    // for (int i = 1; i <= m; i++)
     // {
     //     cout << "R[" << i << "]: " << R[i] << endl;
     // }
 
-
     int ans = 0;
-    for (int i = 0; i < m; i++)
+    for (int i = 1; i <= m; i++)
     {
-        if ((i == 0 && R[1] >= 0) || (i == m - 1 && L[m - 2] < n))
-        {
-            ans++;
-        }
-        else if (L[i - 1] < R[i + 1])
+        if (L[i - 1] < R[i + 1])
         {
             ans++;
         }
