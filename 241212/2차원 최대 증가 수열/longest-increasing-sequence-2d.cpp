7144d02d5ba65ec9b91d6bb099d56e5847f62981
Byunguk Choi <33592844+qowlz@users.noreply.github.com>
2024-12-12 16:54:05 +0900

    Title: 2차원 최대 증가 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241212/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 241212/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -34,20 +34,19 @@ int main() {
     dp.assign(n, vector<int>(m, 0));
     dp[0][0] = 1;
 
+    int ans = 1;
     for (int i = 1; i < n; i++)
     {
         for (int j = 1; j < n; j++)
         {
             int best = getMax(i - 1, j - 1, arr[i][j]);
             dp[i][j] = best > 0 ? best + 1 : 0;
+            ans = max(ans, dp[i][j]);
+
             // cout << "dp[" << i << "][" << j << "]: " << dp[i][j] << endl;
             // cout << "\tbest: " << best << endl;
         }
     }
-
-    int ans = 0;
-    for (int j = 1; j < m; j++)
-        ans = max(ans, dp[n - 1][j]);
     
     cout << ans;
 
